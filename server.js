10240264184e6f2a4dd778df61c28fa53f3bd10e
david115599 <david.bershadsky2@gmail.com>
2020-01-10 14:39:01 -0500

    asd

--- server.js
+++ server.js
@@ -132,11 +132,7 @@ app.get('/game', function(request, response) {
   var logged_in = false;
   fileContentar = fileContent.split(/,|\n/);
 //  console.log(fileContentar);
-<<<<<<< HEAD
-  if (fileContentar.includes(user_data.name) && fileContentar.includes(user_data.password) && fileContentar[fileContentar.indexOf(user_data.name)+1] == user_data.password) {
-=======
-  if (fileContentar.includes(user_data.name) && fileContentar.includes(user_data.password) && fileContentar[fileContentar.indexOf(user_data.name)+1] == user_data.password && fileContentar[fileContentar.indexOf(user_data.name)+1] > 4) {
->>>>>>> parent of 364fcee... asd
+  if (fileContentar.includes(user_data.name) && fileContentar.includes(user_data.password) && fileContentar[fileContentar.indexOf(user_data.name)+1] == user_data.password/* && fileContentar[fileContentar.indexOf(user_data.name)+1] > 4*/) {
     logged_in = true;
   }
 
