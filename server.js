047af4887fa28670a4922f161f15b31f18ce8437
david115599 <david.bershadsky2@gmail.com>
2020-01-23 21:25:47 -0500

    Temporarily dissabled file updating due to it bieng destructive

--- server.js
+++ server.js
@@ -191,7 +191,11 @@ app.post('/:user/game', function(request, response) {
     weapon: request.body.weapon,
     villain: request.body.Villain,
     vallianroll: 0,
-    winner: 0
+    winner: 0,
+    userindex: 0,
+    villainindex: 0,
+    allusers: [],
+    allvillains: []
   };
 
   //  console.log(user_data.weapon);
@@ -201,10 +205,10 @@ app.post('/:user/game', function(request, response) {
   usersar = userss.split(/,|\n/);
   usersarl = userss.split(/\n/);
   var userstats = [];
-  for (var i = 0; i < usersarl.length-1; i++) {
-    userstats[i] = {};
+  for (var i = 1; i < usersarl.length-1; i++) {
+    userstats[i-1] = {};
     for (var q = 0; q < 11; q++) {
-      userstats[i][usersar[q]]=usersar[i*11+q];
+      userstats[i-1][usersar[q]]=usersar[i*11+q];
     }
   }
 
@@ -213,10 +217,10 @@ app.post('/:user/game', function(request, response) {
   fileContentar = fileContent.split(/,|\n/);
   filesortar = fileContent.split(/\n/);
   var villianstats = [];
-  for (var i = 0; i < filesortar.length-1; i++) {
-    villianstats[i] = {};
+  for (var i = 1; i < filesortar.length-1; i++) {
+    villianstats[i-1] = {};
     for (var q = 0; q < 10; q++) {
-      villianstats[i][fileContentar[q]]=fileContentar[i*10+q];
+      villianstats[i-1][fileContentar[q]]=fileContentar[i*10+q];
     }
   }
   index = 0;
@@ -232,15 +236,30 @@ app.post('/:user/game', function(request, response) {
   var scissorsstrat = parseFloat(villianstats[index].scissors_strategy);
   var villainroll=Math.random();
   var villainrollactual = 0;
+  var userindex;
+  var villianindex;
   //  console.log(paperstrat +" "+rockstrat +" "+scissorsstrat +" "+villainroll);
+  for (var i = 0; i < userstats.length; i++) {
+    if (userstats[i].name == user_data.name) {
+      userindex = i;
+    }
+  }
+  for (var i = 0; i < villianstats.length; i++) {
+    if (villianstats[i].name == user_data.villain) {
+      villianindex = i;
+    }
+  }
   if (villainroll<=paperstrat) {
-    villainrollactual=1
+  villainrollactual=1
+  villianstats[villianindex].paper = parseInt(villianstats[villianindex].paper)+1;
   }
   else if (villainroll>paperstrat && villainroll<=(paperstrat+rockstrat)) {
     villainrollactual=2
+  villianstats[villianindex].rock = parseInt(villianstats[villianindex].rock)+1;
   }
   else if (villainroll>(rockstrat+paperstrat)) {
     villainrollactual=3
+  villianstats[villianindex].scissors = parseInt(villianstats[villianindex].scissors)+1;
   }
   else {
     villainrollactual = villainroll
@@ -250,79 +269,95 @@ app.post('/:user/game', function(request, response) {
   var userchoice = 0;
   if (user_data.weapon == "paper") {
     userchoice=1;
+    userstats[userindex].paper = parseInt(userstats[userindex].paper)+1;
   }
   if (user_data.weapon == "rock") {
     userchoice=2;
+    userstats[userindex].rock = parseInt(userstats[userindex].rock)+1;
   }
   if (user_data.weapon == "scissors") {
     userchoice=3;
+    userstats[userindex].scissors = parseInt(userstats[userindex].scissors)+1;
   }
 
   if (userchoice == villainrollactual) {
-    user_data.winner = 0
+    user_data.winner = 0;
+    userstats[userindex].tied = parseInt(userstats[userindex].tied)+1;
+    villianstats[villianindex].tied = parseInt(villianstats[villianindex].tied)+1;
   }
   else if (userchoice == 1 && villainrollactual == 2) {
-    user_data.winner = 1
-    //  fs.writeFile("/data/villains.csv", fileContentar);
+    user_data.winner = 1;
+    userstats[userindex].wins = parseInt(userstats[userindex].wins)+1;
+    villianstats[villianindex].losses = parseInt(villianstats[villianindex].losses)+1;
   }
   else if (userchoice == 1 && villainrollactual == 3) {
-    user_data.winner = 2
+    user_data.winner = 2;
+    userstats[userindex].losses = parseInt(userstats[userindex].losses)+1;
+    villianstats[villianindex].wins = parseInt(villianstats[villianindex].wins)+1;
   }
   else if (userchoice == 2 && villainrollactual == 1) {
-    user_data.winner = 2
+    user_data.winner = 2;
+    userstats[userindex].losses = parseInt(userstats[userindex].losses)+1;
+    villianstats[villianindex].wins = parseInt(villianstats[villianindex].wins)+1;
   }
   else if (userchoice == 2 && villainrollactual == 3) {
-    user_data.winner = 2
+    user_data.winner = 1;
+    userstats[userindex].wins = parseInt(userstats[userindex].wins)+1;
+    villianstats[villianindex].losses = parseInt(villianstats[villianindex].losses)+1;
   }
   else if (userchoice == 3 && villainrollactual == 1) {
-    user_data.winner = 1
+    user_data.winner = 1;
+    userstats[userindex].wins = parseInt(userstats[userindex].wins)+1;
+    villianstats[villianindex].losses = parseInt(villianstats[villianindex].losses)+1;
   }
-  else if (userchoice == 2 && villainrollactual == 2) {
-    user_data.winner = 2
-  }
-
+  user_data.userindex = userindex;
+  user_data.villianindex = villianindex;
+  user_data.allusers = userstats;
+  user_data.allvillains = villianstats;
+console.log(usersarl.length);
+console.log(filesortar.length);
   var temparray = []
   temparray[0] = usersarl[0];
   for (var i = 1; i < usersarl.length-1; i++) {
     var temparray2 = []
     //console.log(temparray.length)
-    temparray2.push(userstats[i].name);
-    temparray2.push(userstats[i].password);
-    temparray2.push(userstats[i].paper);
-    temparray2.push(userstats[i].rock);
-    temparray2.push(userstats[i].scissors);
-    temparray2.push(userstats[i].paper_strategy);
-    temparray2.push(userstats[i].rock_strategy);
-    temparray2.push(userstats[i].scissors_strategy);
-    temparray2.push(userstats[i].wins);
-    temparray2.push(userstats[i].tied);
-    temparray2.push(userstats[i].losses);
+    temparray2.push(userstats[i-1].name);
+    temparray2.push(userstats[i-1].password);
+    temparray2.push(userstats[i-1].paper);
+    temparray2.push(userstats[i-1].rock);
+    temparray2.push(userstats[i-1].scissors);
+    temparray2.push(userstats[i-1].paper_strategy);
+    temparray2.push(userstats[i-1].rock_strategy);
+    temparray2.push(userstats[i-1].scissors_strategy);
+    temparray2.push(userstats[i-1].wins);
+    temparray2.push(userstats[i-1].tied);
+    temparray2.push(userstats[i-1].losses);
     temparray[i] = temparray2.join()
   }
   // = fileContentar
-  fs.writeFileSync('data/users.csv', temparray.join('\n'), 'utf8', function (err) {
-  });
+  //fs.writeFileSync('data/users.csv', temparray.join('\n'), 'utf8', function (err) {
+  //});
 
   temparray = []
   temparray[0] = filesortar[0];
   for (var i = 1; i < filesortar.length-1; i++) {
     var temparray2 = []
     //console.log(temparray.length)
-    temparray2.push(villianstats[i].name);
-    temparray2.push(villianstats[i].paper);
-    temparray2.push(villianstats[i].rock);
-    temparray2.push(villianstats[i].scissors);
-    temparray2.push(villianstats[i].paper_strategy);
-    temparray2.push(villianstats[i].rock_strategy);
-    temparray2.push(villianstats[i].scissors_strategy);
-    temparray2.push(villianstats[i].wins);
-    temparray2.push(villianstats[i].tied);
-    temparray2.push(villianstats[i].losses);
+    temparray2.push(villianstats[i-1].name);
+    temparray2.push(villianstats[i-1].paper);
+    temparray2.push(villianstats[i-1].rock);
+    temparray2.push(villianstats[i-1].scissors);
+    temparray2.push(villianstats[i-1].paper_strategy);
+    temparray2.push(villianstats[i-1].rock_strategy);
+    temparray2.push(villianstats[i-1].scissors_strategy);
+    temparray2.push(villianstats[i-1].wins);
+    temparray2.push(villianstats[i-1].tied);
+    temparray2.push(villianstats[i-1].losses);
     temparray[i] = temparray2.join()
   }
   // = fileContentar
-  fs.writeFileSync('data/villains.csv', temparray.join('\n'), 'utf8', function (err) {
-  });
+  //fs.writeFileSync('data/villains.csv', temparray.join('\n'), 'utf8', function (err) {
+  //});
 
 
   response.status(200);
